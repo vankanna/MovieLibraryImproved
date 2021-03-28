3ae1d776f520e1985fe284b668a401ab3ed05c0a
vankanna <passvankanna@gmail.com>
2021-03-28 12:01:11 -0500

    using class example to make doing assignments and labs more doable

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NLog.Web;
 using System.IO;
+using System.Linq;
 
 namespace MediaLibrary
 {
@@ -13,41 +14,45 @@ namespace MediaLibrary
 
             logger.Info("Program started");
 
-            // Movie movie = new Movie
-            // {
-            //     mediaId = 123,
-            //     title = "Greatest Movie Ever, The (2020)",
-            //     director = "Jeff Grissom",
-            //     // timespan (hours, minutes, seconds)
-            //     runningTime = new TimeSpan(2, 21, 23),
-            //     genres = { "Comedy", "Romance" }
-            // };
-
-            // Console.WriteLine(movie.Display());
-
-            // Album album = new Album
-            // {
-            //     mediaId = 321,
-            //     title = "Greatest Album Ever, The (2020)",
-            //     artist = "Jeff's Awesome Band",
-            //     recordLabel = "Universal Music Group",
-            //     genres = { "Rock" }
-            // };
-            // Console.WriteLine(album.Display());
-
-            // Book book = new Book
-            // {
-            //     mediaId = 111,
-            //     title = "Super Cool Book",
-            //     author = "Jeff Grissom",
-            //     pageCount = 101,
-            //     publisher = "",
-            //     genres = { "Suspense", "Mystery" }
-            // };
-            // Console.WriteLine(book.Display());
-
             string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
-            logger.Info(scrubbedFile);
+            MovieFile movieFile = new MovieFile(scrubbedFile);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // LINQ - Where filter operator & Contains quantifier operator
+            var Movies = movieFile.Movies.Where(m => m.title.Contains("(1990)"));
+            // LINQ - Count aggregation method
+            Console.WriteLine($"There are {Movies.Count()} movies from 1990");
+
+            // LINQ - Any quantifier operator & Contains quantifier operator
+            var validate = movieFile.Movies.Any(m => m.title.Contains("(1921)"));
+            Console.WriteLine($"Any movies from 1921? {validate}");
+
+            // LINQ - Where filter operator & Contains quantifier operator & Count aggregation method
+            int num = movieFile.Movies.Where(m => m.title.Contains("(1921)")).Count();
+            Console.WriteLine($"There are {num} movies from 1921");
+
+            // LINQ - Where filter operator & Contains quantifier operator
+            var Movies1921 = movieFile.Movies.Where(m => m.title.Contains("(1921)"));
+            foreach(Movie m in Movies1921)
+            {
+                Console.WriteLine($"  {m.title}");
+            }
+
+            // LINQ - Where filter operator & Select projection operator & Contains quantifier operator
+            var titles = movieFile.Movies.Where(m => m.title.Contains("Shark")).Select(m => m.title);
+            // LINQ - Count aggregation method
+            Console.WriteLine($"There are {titles.Count()} movies with \"Shark\" in the title:");
+            foreach(string t in titles)
+            {
+                Console.WriteLine($"  {t}");
+            }
+
+            // LINQ - First element operator
+            var FirstMovie = movieFile.Movies.First(m => m.title.StartsWith("Z", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine($"First movie that starts with letter 'Z': {FirstMovie.title}");
+
+            Console.ForegroundColor = ConsoleColor.White;
 
             logger.Info("Program ended");
         }
